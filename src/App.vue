3f2dd677c35dab59eccef88e0055772b3731f4d7
manuelloverso <manuelloverso@gmail.com>
2024-03-20 15:07:43 +0100

    add bonus

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
 <script>
+import MyComponent from "./components/My_component.vue";
+
 export default {
   data() {
     return {
       hello: "Hello Vite",
     };
   },
+
+  components: {
+    MyComponent,
+  },
 };
 </script>
 
 <template>
   <h1>{{ hello }}</h1>
+  <MyComponent />
 </template>
 
 <style></style>
